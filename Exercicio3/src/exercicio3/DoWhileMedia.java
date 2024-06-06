f845a5d17a9a939f8e2bb88657b316fc109bcd9a
Édipo Reis <40582332+EdiporTorres@users.noreply.github.com>
2024-06-06 15:56:30 -0300

    Update DoWhileMedia.java

--- Exercicio3/src/exercicio3/DoWhileMedia.java
+++ Exercicio3/src/exercicio3/DoWhileMedia.java
@@ -10,15 +10,15 @@ public class DoWhileMedia {
 		float  total = 0 , cont=0;
 		Scanner leia = new Scanner(System.in);
 
+
+
+		do { cont++;
 		System.out.println("Digite um numero: ");
 		num = leia.nextInt();
-		C:\Users\edipo\eclipse-workspace
-		do { cont++;
 			if (num % 3 == 0) {
 				total = (num + total) / cont;
 			}
-			System.out.println("Digite um numero: ");
-			num = leia.nextInt();
+
 
 		} while (num != 0);
 		System.out.println("A soma dos numeros positivos é: " + total);
