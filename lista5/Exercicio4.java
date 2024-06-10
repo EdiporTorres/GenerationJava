b88772609cd535fd62c9be621caae3af50ec353f
Édipo Reis <ediporeiss@gmail.com>
2024-06-10 14:34:39 -0300

    Exercicios Collection

---
+++ lista5/Exercicio4.java
@@ -0,0 +1,38 @@
+package lista5;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Exercicio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Set<Integer> num = new HashSet<Integer>();
+		Scanner leia = new Scanner(System.in);
+		num.add(2);
+		num.add(5);
+		num.add(1);
+		num.add(3);
+		num.add(4);
+		num.add(9);
+		num.add(7);
+		num.add(8);
+		num.add(10);
+		num.add(6);
+		for (int X : num) {
+
+			System.out.println("Digite o número que você deseja encontrar: ");
+			int valor = leia.nextInt();
+			if (num.contains(valor)) {
+			
+				System.out.println("O número " + valor + " foi encontrado" );
+			} else {
+				System.out.println("O número"+valor+" não foi encontrado!");
+				break;
+
+			}
+
+		}
+	}
+}
